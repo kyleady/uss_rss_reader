6d45a575b47a5d3d8aadc35b1673d439c9d9a276
Kyle Ady <kyledavidady@gmail.com>
2016-09-12 22:04:12 -0700

    The gemfile and database.yml were modified to work on windows. Beyond this, the first tests were made. Four were made. The Feed and Article models were modified so that at least two of the tests now pass.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,17 @@
 class Article < ApplicationRecord
   belongs_to :feed
+  before_save :clean
+
+  private
+
+  #It is possible for nil values to pop up in Articles, thus it is important to
+  #clean those out
+  def clean
+    title       ||= ''
+    description ||= ''
+    link        ||= ''
+    author      ||= ''
+    guid        ||= ''
+    pub_date    ||= ''
+  end
 end
