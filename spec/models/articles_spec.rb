1a35d747c61aabe0e0f60636e331184d547863f7
Kyle Ady <kyledavidady@gmail.com>
2016-09-22 19:51:06 -0700

    Added tests for the .get functions for both Articles and Feeds

--- spec/models/articles_spec.rb
+++ spec/models/articles_spec.rb
@@ -12,4 +12,38 @@ RSpec.describe Article, type: :model do
       expect(article.viewed?).to eq(true)
     end
   end
+
+  context '.get' do
+    it 'with a nil user, returns nil' do
+      @user = User.new(email: 'fake user', password: 'password')
+      @user.save
+      @feed = Feed.new(url: 'fake feed')
+      @user.feeds << @feed
+      @article = Article.new(title: 'fake article')
+      @feed.articles << @article
+      expect(Article.get(nil, id: @article.id, feed_id: @feed.id)).to eq(nil)
+      @user.destroy
+    end
+    it 'with a non-matching user, returns nil' do
+      @user = User.new(email: 'fake user', password: 'password')
+      @user.save
+      @feed = Feed.new(url: 'fake feed')
+      @user.feeds << @feed
+      @article = Article.new(title: 'fake article')
+      @feed.articles << @article
+      wrong_id = @user.id + 1
+      expect(Article.get(wrong_id, id: @article.id, feed_id: @feed.id)).to eq(nil)
+      @user.destroy
+    end
+    it 'with a matching user, returns the article' do
+      @user = User.new(email: 'fake user', password: 'password')
+      @user.save
+      @feed = Feed.new(url: 'fake feed')
+      @user.feeds << @feed
+      @article = Article.new(title: 'fake article')
+      @feed.articles << @article
+      expect(Article.get(@user.id.to_s, id: @article.id, feed_id: @feed.id)).to eq(@article)
+      @user.destroy
+    end
+  end
 end
--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe Feed, type: :model do
   context 'should belong to a user' do
     it { should belong_to :user }
   end
+
   it { should have_many :articles }
+
   context 'updates a' do
     it 'bogus url' do
       @feed = Feed.new(url: 'bogus url')
@@ -21,6 +23,34 @@ RSpec.describe Feed, type: :model do
     it 'atom feed' do
       @feed = Feed.new(url: 'https://gist.githubusercontent.com/ToddG/1974656/raw/d2d2fd1f7c01b43a67b1c5f39694d9ab5e00903f/sample-atom-1.0-feed.xml')
       expect(@feed.update).to eq(true)
+    end
+  end
+
+  context '.get' do
+    it 'with a nil user, returns nil' do
+      @user = User.new(email: 'fake user', password: 'password')
+      @user.save
+      @feed = Feed.new(url: 'fake feed')
+      @user.feeds << @feed
+      expect(Feed.get(nil, id: @feed.id)).to eq(nil)
+      @user.destroy
+    end
+    it 'with a non-matching user, returns nil' do
+      @user = User.new(email: 'fake user', password: 'password')
+      @user.save
+      @feed = Feed.new(url: 'fake feed')
+      @user.feeds << @feed
+      wrong_id = @user.id+1
+      expect(Feed.get(wrong_id, id: @feed.id)).to eq(nil)
+      @user.destroy
+    end
+    it 'with a matching user, returns the feed' do
+      @user = User.new(email: 'fake user', password: 'password')
+      @user.save
+      @feed = Feed.new(url: 'fake feed')
+      @user.feeds << @feed
+      expect(Feed.get(@user.id.to_s, id: @feed.id.to_s)).to eq(@feed)
+      @user.destroy
     end
   end
 end
