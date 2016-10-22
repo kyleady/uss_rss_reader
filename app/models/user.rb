ec1a22245c39f0642089ac74e95a5c7616f6bc3a
Kyle Ady <kyledavidady@gmail.com>
2016-10-21 19:07:46 -0700

    Removed get_email from the model user. It was unused and untested.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,4 @@ class User < ApplicationRecord
   validates :password, presence: true
   validates :email, uniqueness: :email
 
-  def self.get_email(input_id)
-    if input_id
-      user = User.find(input_id)
-      user.respond_to?('email') ? user.email : ''
-    else
-      ''
-    end
-  end
 end
